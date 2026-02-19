21657fe5721aeafd050cb86ee523823469308a28
Kingfs23 <kingfx982@gmail.com>
2026-02-19 08:28:19 +0100

    supabase.js

--- supabase.js
+++ supabase.js
@@ -4,4 +4,8 @@ const SUPABASE_ANON_KEY = "sb_publishable_fEGUHINYAMku5Bl_N3ebTA_vZcqV_j3";
 // Create client and store it in a different variable name (NOT "supabase")
 window.sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
+console.log("SUPABASE_URL:", sb.supabaseUrl);
+
+
+
 
